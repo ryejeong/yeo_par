94981213f4d0012463be9064b08fb67c84287325
ryejeong <yyuiioop98@gmail.com>
2026-07-22 12:27:10 +0900

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -51,5 +51,5 @@ export default defineConfig({
   ],
   server: { port: 3000 },
   build: { outDir: distFolder },
-  base: process.env.NODE_ENV === 'production' ? '/wedding-par/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/yeo_par/' : '/',
 })
